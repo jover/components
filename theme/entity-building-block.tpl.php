fafb3edaaba1e695f56198bbdb6a27b9c402af0a
Jochen Verdeyen <jochenverdeyen@gmail.com>
2011-06-19 01:52:25 +0200

    Entity Building Block view functionality and default template

--- theme/entity-building-block.tpl.php
+++ theme/entity-building-block.tpl.php
@@ -11,26 +11,36 @@
  *   given element.
  * - $classes: String of classes that can be used to style contextually through
  *   CSS. It can be manipulated through the variable $classes_array from
- *   preprocess functions. By default the following classes are available, where
- *   the parts enclosed by {} are replaced by the appropriate values:
- *   - entity-profile
- *   - profile-{TYPE}
+ *   preprocess functions. The default values can be one or more of the
+ *   following:
+ *   - ebb: The current template type, i.e., "theming hook".
+ *   - ebb-[type]: The current ebb type. For example, if the ebb is a
+ *     "Header ebb" it would result in "ebb-header". Note that the machine
+ *     name will often be in a short form of the human readable label.
+ *   - ebb-unpublished: Unpublished ebbs visible only to administrators.
  *
  * Other variables:
+ * - $ebb: Full ebb object. Contains data that may not be safe.
+ * - $type: Ebb type, i.e. header, paragraph, etc.
  * - $classes_array: Array of html class attribute values. It is flattened
  *   into a string within the variable $classes.
+ * 
+ * Ebb status variables:
+ * - $view_mode: View mode, e.g. 'full', 'teaser'...
+ * - $status: Flag for published status.
+ * 
+ * Field variables: for each field instance attached to the ebb a corresponding
+ * variable is defined, e.g. $ebb->body becomes $body. When needing to access
+ * a field's raw values, developers/themers are strongly encouraged to use these
+ * variables. Otherwise they will have to explicitly specify the desired field
+ * language, e.g. $ebb->body['en'], thus overriding any language negotiation
+ * rule that was previously applied.
  *
  * @see template_preprocess()
  * @see template_preprocess_entity()
  * @see template_process()
  */
 ?>
-<div class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
-
-  <div class="content"<?php print $content_attributes; ?>>
-    <?php
-      print render($content);
-    ?>
-  </div>
-  
+<div id="ebb-<?php print $ebb->ebbid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+  <?php print render($content); ?>
 </div>
