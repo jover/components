3d066dd3655f56a3f9401a8a6b66b5c122a66195
Jochen Verdeyen <jochenverdeyen@gmail.com>
2011-12-18 01:50:41 +0100

    Added field & display overview functionality: weight & visibility settings

--- modules/components_group/theme/components-group.tpl.php
+++ modules/components_group/theme/components-group.tpl.php
@@ -1,4 +1,9 @@
 <div id="<?php print $id; ?>" class="<?php print implode(' ', $classes_array); ?>">
+  <?php if ($components_group_add_link): ?>
+    <div class"components-group-add-link">
+      <?php print render($components_group_add_link); ?>
+    </div>
+  <?php endif; ?>
   <?php if ($components): ?>
     <div class="components">
       <?php foreach ($components as $component): ?>
